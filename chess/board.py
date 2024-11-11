ecc4be4a77a809695f720cb2c4ee37f241e7643e
spcaldicpt024 <spcaldicpt024@student.wethinkcode.co.za>
2024-11-11 10:00:38 +0200

    All for now

--- chess/board.py
+++ chess/board.py
@@ -4,7 +4,11 @@ from .piece import Piece
 
 
 class Board:
-    
+
+
+    """
+    __x__ type functions
+    """
     def __init__(self, fen_string):
             info = fen_string.split(' ')
             rows = info[0].split('/')
@@ -16,23 +20,14 @@ class Board:
             self.fullmove = info[5]
 
 
-    def writerow(self, row, row_num):
-        temp = []
-        for character in row:
-            if character.isdigit():
-                temp += '.'*int(character)
-            else:
-                temp += character
-
-        output = []
-        for col, character in enumerate(temp):
-            if character == '.':
-                output.append(character)
-            else:
-                output.append(Piece(row_num, col, character))
-        
+    def __str__(self):
+        output = self.get_game_state_string()
         return output
 
+
+    """
+    Accessor methods
+    """
 
     def get_turn(self):
         return self.turn
@@ -51,16 +46,26 @@ class Board:
             return (4, self.get_en_passant()[0])
 
 
-    def set_en_passant(self, new_en_passant):
-        self.en_passant = new_en_passant
-
-
-    def get_board_string(self):
-        output = ""
+    def get_board_list(self):
+        output = []
         for row in self.board:
-            output += ' '.join(row) + '\n'
+            temp = []
+            for item in row:
+                if item == '.':
+                    temp.append('.')
+                elif type(item) == Piece:
+                    temp.append(item.get_piece_code())                
+            output.append(temp)
         return output
     
+
+    def get_board_string(self):
+        board_list = self.get_board_list()
+        output = ''
+        for row in board_list:
+            output += ' '.join(row) + '\n'
+        return output    
+
 
     def get_turn_string(self):
         if self.turn == 'w':
@@ -90,28 +95,12 @@ class Board:
                 return 'Black cannot castle'
 
 
-    def castle_string_to_list(self, s):
-        return ['K' in s, 'Q' in s, 'k' in s, 'q' in s]
-
-
     def get_castle(self, colour):
         if colour == WHITE:
             return (self.castle[0], self.castle[1])
         else:
             return (self.castle[2], self.castle[3])
-        
-
-    def string_to_coords(self, string):
-        if string == '-':
-            return False
-        col = 'abcdefgh'.find(string[0])
-        row = (8-int(string[1]))
-        return (row, col)
-
-    def coords_to_string(self, coords):
-        row = str(8 - coords[0])
-        col = 'abcdefgh'[coords[1]]
-        return col + row
+    
 
     def get_en_passant_string(self):
         if self.en_passant == False:
@@ -150,23 +139,28 @@ class Board:
         return output
 
 
-    def __str__(self):
-        output = self.get_game_state_string()
-        return output
-
-
-    def draw_squares(self,win):
-        win.fill(SQUARE_1)
-        for row in range(ROWS):
-            for col in range(row % 2 , ROWS, 2):
-                pygame.draw.rect(win, SQUARE_2, (row*SQUARE_SIZE , col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    def get_piece(self, row, col):
+        return self.board[row][col]
     
 
-    def _en_passant_used(self, row, col):
-        if (row, col) == self.get_en_passant():
-            to_remove = self.get_en_passant_to_remove()
-            self.remove(self.get_piece(to_remove[0], to_remove[1]))
+    """
+    Mutator methods
+    """
+    def _move(self, piece : Piece, row, col):
+        if self.get_piece(row, col) != '.':
+            self.remove(self.get_piece(row, col))
+        self.board[row][col],self.board[piece.row][piece.col] = self.board[piece.row][piece.col], '.'
+        piece.move(row, col)
 
+
+    def move(self, piece : Piece, row, col):
+        self._check_en_passant(piece, row, col)
+        self._check_castle(piece, row, col)
+        self._move(piece, row, col)
+        if (row == ROWS - 1 or row == 0) and piece.get_piece_type() == 'Pawn':
+            pass
+            #TODO make it so pawns can become other piece when touching opposite end
+    
 
     def _set_new_en_passant(self, piece : Piece, row):
         if abs(piece.get_row() - row) == 2:
@@ -176,14 +170,15 @@ class Board:
             self.set_en_passant(False)
 
 
-    def _check_en_passant(self, piece : Piece, row, col):
-        if piece.get_piece_type() != 'Pawn':
-            self.set_en_passant(False)
-            return
-        
-        self._en_passant_used(row, col)
-        self._set_new_en_passant(piece, row)
-    
+    def remove(self, piece : Piece):
+        self.board[piece.row][piece.col] = '.'
+        #TODO keep track of the chess score of each player
+
+
+    def set_en_passant(self, new_en_passant):
+        self.en_passant = new_en_passant
+
+
     def _do_castle(self, piece : Piece, row, col):
         if abs(piece.get_col() - col) == 2:
             if col == 6:
@@ -193,6 +188,95 @@ class Board:
             return True
         return False
 
+
+    def _en_passant_used(self, row, col):
+        if (row, col) == self.get_en_passant():
+            to_remove = self.get_en_passant_to_remove()
+            self.remove(self.get_piece(to_remove[0], to_remove[1]))
+
+
+    """
+    Converter methods
+    """
+    def writerow(self, row, row_num):
+        temp = []
+        for character in row:
+            if character.isdigit():
+                temp += '.'*int(character)
+            else:
+                temp += character
+
+        output = []
+        for col, character in enumerate(temp):
+            if character == '.':
+                output.append(character)
+            else:
+                output.append(Piece(row_num, col, character))
+        
+        return output
+
+
+    def castle_string_to_list(self, s):
+        return ['K' in s, 'Q' in s, 'k' in s, 'q' in s]
+
+
+    def string_to_coords(self, string):
+        if string == '-':
+            return False
+        col = 'abcdefgh'.find(string[0])
+        row = (8-int(string[1]))
+        return (row, col)
+
+
+    def coords_to_string(self, coords):
+        row = str(8 - coords[0])
+        col = 'abcdefgh'[coords[1]]
+        return col + row
+
+
+    def transform(self, start_pos, row_col):
+        row = start_pos[0] + row_col[0] 
+        col = start_pos[1] + row_col[1]
+        return row, col
+ 
+
+    def code_to_colour(self, piece_code):
+        if piece_code == '.':
+            return False
+        if piece_code.upper() == piece_code:
+            return WHITE
+        return BLACK
+
+    """
+    Graphical methods
+    """
+    def draw_squares(self,win):
+        win.fill(SQUARE_1)
+        for row in range(ROWS):
+            for col in range(row % 2 , ROWS, 2):
+                pygame.draw.rect(win, SQUARE_2, (row*SQUARE_SIZE , col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    
+
+    def draw(self, win):
+        self.draw_squares(win)
+        for row in range(ROWS):
+            for col in range(COLS):
+                piece = self.board[row][col]
+                if piece != '.':
+                    piece.draw(win)
+ 
+
+    """
+    Checker methods
+    """
+    def _check_en_passant(self, piece : Piece, row, col):
+        if piece.get_piece_type() != 'Pawn':
+            self.set_en_passant(False)
+            return
+        
+        self._en_passant_used(row, col)
+        self._set_new_en_passant(piece, row)
+    
 
     def _check_castle(self, piece : Piece, row, col):
 
@@ -216,41 +300,27 @@ class Board:
                     self.castle[2] = False
                 elif piece.get_col() == 0:
                     self.castle[3] = False
+   
 
+    def is_on_board(self, row, col):
+        if row < 0 or row >= ROWS:
+            return False
+        if col < 0 or col >= COLS:
+            return False 
+        return True
+ 
 
-    def _move(self, piece : Piece, row, col):
-        if self.get_piece(row, col) != '.':
-            self.remove(self.get_piece(row, col))
-        self.board[row][col],self.board[piece.row][piece.col] = self.board[piece.row][piece.col], '.'
-        piece.move(row, col)
-
-
-    def move(self, piece : Piece, row, col):
-        self._check_en_passant(piece, row, col)
-        self._check_castle(piece, row, col)
-        self._move(piece, row, col)
-        if (row == ROWS - 1 or row == 0) and piece.get_piece_type() == 'Pawn':
-            pass
-            #TODO make it so pawns can become other piece when touching opposite end
+    def _check_board_given(self, board):
+        if not board:
+            output = self.get_board_list()
+        else:
+            output = board
+        return board
     
 
-    def get_piece(self, row, col):
-        return self.board[row][col]
-    
-
-    def draw(self, win):
-        self.draw_squares(win)
-        for row in range(ROWS):
-            for col in range(COLS):
-                piece = self.board[row][col]
-                if piece != '.':
-                    piece.draw(win)
-    
-
-    def remove(self, piece : Piece):
-        self.board[piece.row][piece.col] = '.'
-        #TODO keep track of the chess score of each player
-
+    """
+    Finding valid moves methods
+    """
 
     def get_valid(self, piece : Piece):
         coords = piece.get_pos()
@@ -271,22 +341,9 @@ class Board:
                 moves = self._get_king_moves(coords, colour) 
             
         return moves
-    
-    def is_on_board(self, row, col):
-        if row < 0 or row >= ROWS:
-            return False
-        if col < 0 or col >= COLS:
-            return False 
-        return True
-    
 
-    def transform(self, start_pos, row_col):
-        row = start_pos[0] + row_col[0] 
-        col = start_pos[1] + row_col[1]
-        return row, col
-    
 
-    def _traverse(self, start_coords, colour, continuos, transformations):
+    def _traverse(self, start_coords, colour, continuos, transformations, board):
         moves = []
 
         for transformation in transformations:
@@ -298,16 +355,16 @@ class Board:
                 if not self.is_on_board(row, col):
                     break
                 
-                current = self.board[row][col]
+                current = board[row][col]
 
                 if current == '.':
                     moves.append( (row, col) )
                     working_coords = (row, col)
 
-                elif current.get_colour() == colour:
+                elif self.code_to_colour(current) == colour:
                     break
 
-                elif current.get_colour() != colour:
+                elif self.code_to_colour(current) != colour:
                     moves.append( (row, col) )
                     break
                 
@@ -317,26 +374,28 @@ class Board:
         return moves
     
 
-    def _traverse_diagonal(self, start_coords, colour, continuos):
+    def _traverse_diagonal(self, start_coords, colour, continuos, board=False):
+        board = self._check_board_given(board)
         transformations = [(-1,-1) ,(-1,1) ,(1,-1) ,(1,1)]
-        return self._traverse(start_coords, colour, continuos, transformations)
+        return self._traverse(start_coords, colour, continuos, transformations, board)
 
 
-    def _traverse_vertical(self, start_coords, colour, continuos):
+    def _traverse_vertical(self, start_coords, colour, continuos, board=False):
+        board = self._check_board_given(board)
         transformations = [(-1, 0), (1, 0)]
-        return self._traverse(start_coords, colour, continuos, transformations)
+        return self._traverse(start_coords, colour, continuos, transformations, board)
 
 
-
-    def _traverse_horizontal(self, start_coords, colour, continuos):
+    def _traverse_horizontal(self, start_coords, colour, continuos, board=False):
+        board = self._check_board_given(board)
         transformations = [(0,1),(0,-1)]  
-        return self._traverse(start_coords, colour, continuos, transformations)
+        return self._traverse(start_coords, colour, continuos, transformations, board)
 
 
-    def _traverse_knight(self, start_coords, colour):
-        moves = []
+    def _traverse_knight(self, start_coords, colour, board=False):
+        board = self._check_board_given(board)
         transformations = [(1, 2) ,(-1, 2) ,(1, -2) ,(-1, -2) ,(2, 1) ,(-2, 1) ,(2, -1) ,(-2, -1)]
-        moves += self._traverse(start_coords, colour, False, transformations)
+        moves = self._traverse(start_coords, colour, False, transformations, board)
         
         return moves
 
@@ -347,7 +406,9 @@ class Board:
             row, col = self.transform(start_coords, transformation)
             current = self.board[row][col]
             if current == '.':
-                moves.append( (row, col) )
+                moves.append((row, col))
+            else:
+                break
         return moves
 
 
@@ -365,8 +426,6 @@ class Board:
             elif current.get_colour() != colour:
                 moves.append( coords ) 
         return moves
-
-
 
 
     def _traverse_castling(self, colour):
